435ed807f6951e72d067b9505705f1dc1fe232af
Christoph Bünte <info@christophbuente.de>
2009-09-29 14:40:36 +0200

    added rdoc

--- lib/dawanda/category.rb
+++ lib/dawanda/category.rb
@@ -2,7 +2,9 @@ module Dawanda
   
   # = Category
   #
-  # Represents a single Dawanda category - has the following attributes:
+  # Dawanda::Category are organized in a tree. To get the top level categories call top
+  #
+  # Represents a single Dawanda::Category - has the following attributes:
   #
   # [id] The unique identifier for this category
   # [parent_id] This category's parent id
@@ -19,19 +21,26 @@ module Dawanda
     
     attributes :id, :parent_id, :name, :product_count
     
+    # Get all products for this category.
     def products(params = {})
       @products ||= Product.find_all_by_category_id(id, params)
     end
     
+    # Get all the top level categories.
     def self.top(params = {})
       # This is a hack, because dynamic finder generation takes at least
       # one placeholder like ":name"
       self.find_all_by_name('top', params)
     end
     
+    # Get all child categories of this category
     def children(params = {})
       Category.find_all_by_parent_id(id, params)
     end
-    
+
+    # Get the parent category of this category
+    def parent(params = {})
+      (parent_id and parent_id !=0) ? Category.find_by_id(parent_id, params) : nil
+    end
   end
 end
